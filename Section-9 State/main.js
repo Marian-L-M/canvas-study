8db05a0c0a38f46b8e5913d55603bb268d562a42
MarianMaschkeRGST <m.maschke@rgst.net>
2022-05-16 23:12:19 +0900

    finished state lesson

--- Section-9 State/main.js
+++ Section-9 State/main.js
@@ -12,13 +12,30 @@ window.onload = function(){
     // Rect 1
     context.fillStyle='red';
     context.fillRect(240, 100, 120, 120);
+    context.save();
+    
     
     // Rect 2
     context.fillStyle='green';
     context.fillRect(390, 100, 120, 120);
+    context.save();
     
     // Rect 3
     context.fillStyle='blue';
     context.fillRect(540, 100, 120, 120);
+    context.save();
     
+    // Rect 4
+    context.restore();
+    context.fillRect(240, 300, 120, 120);
+    
+    // Rect 5
+    context.restore();
+    context.fillRect(390, 300, 120, 120);
+    
+    // Rect 6
+    context.restore();
+    context.fillRect(540, 300, 120, 120);
+
+    // Order of restore state (here fill style) is LIFO/FILO e.g. first restored would be blue-4, green-5, red-6 
 };
