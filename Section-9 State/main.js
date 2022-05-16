c809f519040e398c98f91d9e921213c8d2df5c75
MarianMaschkeRGST <m.maschke@rgst.net>
2022-05-16 23:06:44 +0900

    fixed html  file mixup

--- Section-9 State/main.js
+++ Section-9 State/main.js
@@ -1,4 +1,24 @@
-const canvas = document.getElementById("hello-world-canvas");
+window.onload = function(){
+    
+    //　Definitions
+    const canvas = document.getElementById("le-canvas");
+    const context = canvas.getContext("2d")
+    
+    
+    // State Stack
+    // context.save() -> saving the context state-canvas
+    // context.restore() -> restoring the state from the state stack
 
-const context = canvas.getContext("2d")
-
+    // Rect 1
+    context.fillStyle='red';
+    context.fillRect(240, 100, 120, 120);
+    
+    // Rect 2
+    context.fillStyle='green';
+    context.fillRect(390, 100, 120, 120);
+    
+    // Rect 3
+    context.fillStyle='blue';
+    context.fillRect(540, 100, 120, 120);
+    
+};
