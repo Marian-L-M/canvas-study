ea15c2bab36a18585c52ca6e2107a97e8d3b8a34
Marian-L-M <marianlmaschke@gmail.com>
2022-05-24 23:21:40 +0900

    finished sprite animation

--- Project_3_sprite_animation/main.js
+++ Project_3_sprite_animation/main.js
@@ -1,5 +1,5 @@
-// Concept for animation in canvas:
-// Update-> clear -> draw (repeat)
+// Concept one image file
+// For animation a different part of the image is shown with each iteration
 
 window.onload = function(){
     
@@ -11,9 +11,53 @@ window.onload = function(){
     tile.src = 'tile.png';
 
     tile.onload = function () {
-        drawImage(tile, 0, 0, 768, 200, 60, 0, 768, 200)
+        context.drawImage(tile, 0, 0, 768, 200, 60, 0, 768, 200)
+        
     };
 
+    let cellWidth = 192;
+    let cellHeight = 200;
 
-    window.requestAnimationFrame
+
+    function drawTileCell(index) {
+        context.drawImage(tile, index * cellWidth, 0, cellWidth, cellHeight, 360, 300,cellWidth, cellHeight);
+        
+    }
+    window.requestAnimationFrame(animationLoop);
+
+    let cell = 0; 
+    let start = new Date();
+
+    // For stability it might be smarter to put animation loop into tile.onload
+    function animationLoop(){
+        let now = new Date();
+        if(now - start >= 150){
+            start = now;
+            
+            // Clear
+            context.clearRect(0, 200, canvas.width, canvas.height - 200);
+            
+            // Update cell number
+            cell++;
+            cell %= 4;
+            
+            // Draw
+            drawTileCell(cell)
+        }
+            
+        //Animate
+        window.requestAnimationFrame(animationLoop)
+
+    }
+
+
+    window.requestAnimationFrame = (function(){
+        return window.requestAnimationFrame
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+                function(callback){
+                    window.setTimeout(callback, 1000/60);
+                };
+    })();
 };
