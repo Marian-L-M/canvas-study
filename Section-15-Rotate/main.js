2d2b448407148cd33ab225ef801f6187a756ceae
Marian-L-M <marianlmaschke@gmail.com>
2022-06-01 23:18:14 +0900

    translate function

--- Section-15-Rotate/main.js
+++ Section-15-Rotate/main.js
@@ -7,18 +7,37 @@ window.onload = function(){
     const canvas = document.getElementById("le-canvas");
     const context = canvas.getContext("2d");
 
-    const radian = Math.PI / 180;
-
-    // context.rotate(radian * degree); -> rotates in clockwise direction
-
-    // Rotate
-    // context.rotate(radian * 10);
-    context.rotate(radian * 50); // Will mostly disappear -> rotation point is top left of canvas (Canvas origin). 
+    // // ======== Rotate ===========
+    // const radian = Math.PI / 180;
     
+    // // context.rotate(radian * degree); -> rotates in clockwise direction
+    
+    // // Rotate
+    // // context.rotate(radian * 10);
+    // context.rotate(radian * 50); // Will mostly disappear -> rotation point is top left of canvas (Canvas origin). 
+    
+    // // To change rotation point, canvas origin neeeds to be translated
+    
+    
+    // // Rectangle
+    // context.fillStyle='red';
+    // context.fillRect(250, 250, 400, 160);
+    // context.fill();
+    
+    // // ======== Translate ===========
 
-    // Rectangle
+    // Origin Rectangle
     context.fillStyle='red';
-    context.fillRect(250, 250, 400, 160);
+    context.fillRect(50, 50, 160, 160);
+    context.fill();
+    
+    // Translate Canvas
+    context.translate(300, 200);
+
+    // Translated Rectangle
+    // Will not simply move the rectangle, but change the origin point of the canvas
+    context.fillStyle='blue';
+    context.fillRect(50, 50, 160, 160);
     context.fill();
     
 };
